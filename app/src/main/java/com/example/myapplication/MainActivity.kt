7bfd953ca2d14b9e39037ec59d3d6c057970471e
Hank Warren <hwarren@leviton.com>
2021-06-23 19:52:36 -0700

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,117 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                MyScreenContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MaterialTheme {
+        Surface(color = Color.Green) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }) {
+    val counterState = remember { mutableStateOf(0) }
+
+    Column( modifier = Modifier.fillMaxHeight() ) {
+        Image(
+            painter = painterResource(id = R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(Modifier.height(15.dp))
+
+        Text(
+            "A day wandering through the Sandhills " +
+                    " in Shark Fine Cove, and a few of the " +
+                    "sights I see.",
+            style = typography.h6,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Surface(color = Color.Red) {
+            Text(
+                "Davenport, California",
+                style = typography.body2
+            )
+        }
+        Text(
+            "December 2018",
+            style = typography.body2
+        )
+        NameList(names, Modifier.weight(1f))
+        Counter()
+    }
+}
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
+            Greeting(name=name)
+            Divider(color=Color.Black)
+        }
+    }
+}
+@Composable
+fun Greeting(name: String) {
+    var isSelected by remember { mutableStateOf(false)}
+    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+    Text(text="Hello $name",
+        modifier = Modifier
+            .padding(24.dp)
+            .background(color = backgroundColor)
+            .clickable(onClick = {isSelected = !isSelected}))
+}
+
+@Composable
+fun Counter() {
+    val count = remember { mutableStateOf(0) }
+    Button(onClick = { count.value++ }) {
+        Text("I've been clicked ${count.value} times")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyScreenContent()
+}
